f6e65f33e8d422d62a44f6a7b4acff2e070cdc24
mefji <banmefji@gmail.com>
2025-04-02 11:37:47 +0300

    Added Bulk-Converter

---
+++ baseConverter.py
@@ -0,0 +1,11 @@
+class BaseConverter:
+    def __init__(self, converters):
+        self.converters = converters
+
+    def convert(self, fromUnit, amount, toUnit, converterName):
+
+        neededConverter = self.converters[converterName]
+
+        value_in_base = amount / neededConverter[fromUnit]
+
+        return {toUnit: value_in_base * neededConverter[toUnit]}
